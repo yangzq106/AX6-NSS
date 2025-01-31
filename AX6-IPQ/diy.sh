d561405ba2d78348d3f8a50dc3ba27fb673e4940
kokoipz <106084614+kokoipz@users.noreply.github.com>
2025-01-31 19:16:55 +0800

    Update diy.sh

--- AX6-IPQ/diy.sh
+++ AX6-IPQ/diy.sh
@@ -13,19 +13,25 @@ function git_sparse_clone() {
 #添加科学上网源
 #git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall-packages
 #git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
-git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+#git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+#git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 #git clone --depth 1 https://github.com/sirpdboy/luci-app-ddns-go package/ddnsgo
 #git clone --depth 1 https://github.com/sbwml/luci-app-alist package/alist
 #git clone --depth=1  https://github.com/kenzok8/small-package package/small-package
 #git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-zerotier
-git_sparse_clone main https://github.com/kiddin9/kwrt-packages vlmcsd
-git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-vlmcsd
-git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-turboacc
+#git_sparse_clone main https://github.com/kiddin9/kwrt-packages vlmcsd
+#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-vlmcsd
 git_sparse_clone main https://github.com/kiddin9/kwrt-packages qosmate
 git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-qosmate
 #git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-socat
 git_sparse_clone dev https://github.com/vernesong/OpenClash luci-app-openclash
+git_sparse_clone qualcommax-6.x-nss-wifi https://github.com/TerryLip/AX6NSS/package/new luci-theme-argon
+git_sparse_clone qualcommax-6.x-nss-wifi https://github.com/TerryLip/AX6NSS/package/new luci-app-argon-config
+git_sparse_clone qualcommax-6.x-nss-wifi https://github.com/TerryLip/AX6NSS/package/new luci-app-turboacc
+git_sparse_clone qualcommax-6.x-nss-wifi https://github.com/TerryLip/AX6NSS/package/new vlmcsd
+git_sparse_clone qualcommax-6.x-nss-wifi https://github.com/TerryLip/AX6NSS/package/new luci-app-vlmcsd
+
+
 
 
 # mosdns
