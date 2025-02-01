2a8f73e037045bd0de00aab717e3b94e00e259bd
kokoipz <106084614+kokoipz@users.noreply.github.com>
2025-02-01 12:21:09 +0800

    Update diy.sh

--- AX6-IPQ/diy.sh
+++ AX6-IPQ/diy.sh
@@ -25,6 +25,7 @@ git clone --depth 1 https://github.com/hudra0/luci-app-qosmate package/luci-app-
 #git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-vlmcsd
 #git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-socat
 git_sparse_clone dev https://github.com/vernesong/OpenClash luci-app-openclash
+#拉取TerryLip大佬部分源码，感谢大佬对nss的支持！
 git_sparse_clone qualcommax-6.x-nss-wifi https://github.com/TerryLip/AX6NSS package/new/luci-theme-argon
 git_sparse_clone qualcommax-6.x-nss-wifi https://github.com/TerryLip/AX6NSS package/new/luci-app-argon-config
 git_sparse_clone qualcommax-6.x-nss-wifi https://github.com/TerryLip/AX6NSS package/new/luci-app-turboacc
