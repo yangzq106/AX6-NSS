55cf844657ea54951f63f5b4c2e44cde53599868
kokoipz <106084614+kokoipz@users.noreply.github.com>
2025-02-20 21:50:03 +0800

    Update diy.sh

--- AX6-lEDE/diy.sh
+++ AX6-lEDE/diy.sh
@@ -11,40 +11,67 @@ function git_sparse_clone() {
 
 # Add packages
 #添加科学上网源
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall-packages
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
-git clone -b 18.06 --single-branch --depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-git clone -b 18.06 --single-branch --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
-#git clone --depth=1 https://github.com/sirpdboy/luci-app-ddns-go package/ddnsgo
-#git clone -b v5-lua --single-branch --depth 1 https://github.com/sbwml/luci-app-mosdns package/mosdns
-#git clone -b v3.35.0 --single-branch --depth 1 https://github.com/sbwml/luci-app-alist package/alist
-#git clone --depth=1 https://github.com/sirpdboy/luci-app-lucky package/lucky
-#git clone --depth=1 https://github.com/sirpdboy/NetSpeedTest package/NetSpeedTest
-#git clone --depth=1 https://github.com/sirpdboy/NetSpeedTest package/NetSpeedTest
-#git clone --depth=1 https://github.com/0x676e67/luci-theme-design package/luci-theme-design
-#git clone --depth=1 https://github.com/0x676e67/luci-app-design-config package/luci-app-design-config
+#git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall-packages
+#git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
+git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+git clone --depth 1 https://github.com/hudra0/qosmate package/qosmate
+git clone --depth 1 https://github.com/hudra0/luci-app-qosmate package/luci-app-qosmate
+#git clone --depth 1 https://github.com/sirpdboy/luci-app-ddns-go package/ddnsgo
+#git clone --depth 1 https://github.com/sbwml/luci-app-alist package/alist
+#git clone --depth=1  https://github.com/kenzok8/small-package package/small-package
+#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-zerotier
+#git_sparse_clone main https://github.com/kiddin9/kwrt-packages vlmcsd
+#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-vlmcsd
+git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-subconverter
+git_sparse_clone dev https://github.com/vernesong/OpenClash luci-app-openclash
+#拉取TerryLip大佬部分源码，感谢大佬对nss的支持！
+#git_sparse_clone qualcommax-6.x-nss-wifi https://github.com/TerryLip/AX6NSS package/new/luci-theme-argon
+#git_sparse_clone qualcommax-6.x-nss-wifi https://github.com/TerryLip/AX6NSS package/new/luci-app-argon-config
+git_sparse_clone qualcommax-6.x-nss-wifi https://github.com/TerryLip/AX6NSS package/new/luci-app-turboacc
+git_sparse_clone qualcommax-6.x-nss-wifi https://github.com/TerryLip/AX6NSS package/new/vlmcsd
+git_sparse_clone qualcommax-6.x-nss-wifi https://github.com/TerryLip/AX6NSS package/new/luci-app-vlmcsd
 
-#添加自定义的软件包源
-#git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-theme-design
-#git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-design-config
+# mosdns
+git_sparse_clone v5 https://github.com/sbwml/luci-app-mosdns luci-app-mosdns
+git_sparse_clone v5 https://github.com/sbwml/luci-app-mosdns mosdns
+git_sparse_clone v5 https://github.com/sbwml/luci-app-mosdns v2dat
 
+# 替换luci-app-openvpn-server imm源的启动不了服务！
+#rm -rf feeds/luci/applications/luci-app-openvpn-server
+#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-openvpn-server
+# 调整 openvpn-server 到 VPN 菜单
+#sed -i 's/services/vpn/g' package/luci-app-openvpn-server/luasrc/controller/*.lua
+#sed -i 's/services/vpn/g' package/luci-app-openvpn-server/luasrc/model/cbi/openvpn-server/*.lua
+#sed -i 's/services/vpn/g' package/luci-app-openvpn-server/luasrc/view/openvpn/*.htm
 
-# Remove packages
-#删除lean库中的插件，使用自定义源中的包。
-#rm -rf feeds/packages/net/v2ray-geodata
-#rm -rf feeds/packages/utils/v2dat
+#git clone -b js https://github.com/papagaye744/luci-theme-design package/luci-theme-design
+
+#替换luci-app-socat为https://github.com/chenmozhijin/luci-app-socat
+#rm -rf feeds/luci/applications/luci-app-socat
+#git_sparse_clone main https://github.com/chenmozhijin/luci-app-socat luci-app-socat
+
+#删除库中的插件，使用自定义源中的包。
+rm -rf feeds/luci/applications/luci-app-openclash
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/applications/luci-app-argon-config
-#rm -rf feeds/packages/net/mosdns
-#rm -rf feeds/luci/applications/luci-app-mosdns
-#rm -rf feeds/luci/themes/luci-theme-design
-#rm -rf feeds/luci/applications/luci-app-design-config
+rm -rf feeds/packages/net/vlmcsd
+rm -rf feeds/luci/applications/luci-app-vlmcsd
+#rm -rf feeds/luci/applications/luci-app-ddns-go
+#rm -rf feeds/packages/net/ddns-go
+#rm -rf feeds/packages/net/alist
+#rm -rf feeds/luci/applications/luci-app-alist
+#rm -rf feeds/luci/applications/openwrt-passwall
+#rm -rf feeds/luci/applications/openwrt-passwall-packages
 
-# Default IP
-#sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+#添加istore
+echo >> feeds.conf.default
+echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
+./scripts/feeds update istore
+./scripts/feeds install -d y -p istore luci-app-store
 
-# 修改主机名
-#sed -i "s/hostname='OpenWrt'/hostname='Redmi-AX6'/g" package/base-files/files/bin/config_generate
+#修改默认IP
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
-#修改默认时间格式
-sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S %A")/g' $(find ./package/*/autocore/files/ -type f -name "index.htm")
+#修改主机名
+#sed -i "s/hostname='ImmortalWrt'/hostname='Redmi-AX6'/g" package/base-files/files/bin/config_generate
